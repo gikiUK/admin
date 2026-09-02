930d6a12d11a7bc859dec4c1682c4019d74585a8
dem4ron <demaaron88@gmail.com>
2026-09-02 16:18:23 +0200

    Add ability to change existing org flags

--- lib/manage/api.ts
+++ lib/manage/api.ts
@@ -41,6 +41,7 @@ export function fetchCompany(slug: string): Promise<{ company: ManagedCompany }>
 export type UpdateCompanyPayload = {
   gifted_premium_until?: string | null;
   name?: string;
+  feature_flags_enabled_until?: string | null;
 };
 
 export function updateCompany(slug: string, payload: UpdateCompanyPayload): Promise<{ company: ManagedCompany }> {
